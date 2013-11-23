1d61262c367f7c172007a84084b845ed6b04daf5
wxppt <wx_ppt@126.com>
2013-11-23 18:57:13 +0800

    写了一些数据层的东西

---
+++ src/main/java/NJU/HouseWang/nju_eas_server/data/CourseList.java
@@ -0,0 +1,143 @@
+package NJU.HouseWang.nju_eas_server.data;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import NJU.HouseWang.nju_eas_server.po.User.GuestPO;
+import NJU.HouseWang.nju_eas_server.systemMessage.Feedback;
+import NJU.HouseWang.nju_eas_server.systemMessage.UserType;
+
+public class CourseList {
+	private String listName = "course_list";
+	private String sql = null;
+	private SQLConnector sqlconn = new SQLConnector();
+	private Connection conn = null;
+	private PreparedStatement ps = null;
+	private ResultSet rs = null;
+
+	// 连接数据库
+	public void init() {
+		try {
+			sqlconn.createConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// 关闭数据库
+	public void finish() {
+		try {
+			sqlconn.shutdownConnection();
+			if (rs != null) {
+				rs.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// 是否包含ID
+	public boolean containsID(String id) {
+		Boolean result = false;
+		sql = "select id from " + listName + " where id=?;";
+		try {
+			conn = sqlconn.getConnection();
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, id);
+			rs = ps.executeQuery();
+			result = (rs.next() != false);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public GuestPO getData(String id) {
+		GuestPO result = new GuestPO();
+		sql = "select * from " + listName + " where id=?";
+		try {
+			conn = sqlconn.getConnection();
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, id);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				result.setId(rs.getString(1));
+				result.setType(UserType.valueOf(rs.getString(2)));
+				result.setPassword(rs.getString(3));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public Feedback removeData(String id) {
+		sql = "delete from " + listName + " where id=?";
+		try {
+			conn = sqlconn.getConnection();
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, id);
+			ps.execute();
+			return Feedback.OPERATION_SUCCEED;
+		} catch (SQLException e) {
+			// e.printStackTrace();
+			return Feedback.OPERATION_FAIL;
+		}
+	}
+
+	public Feedback addData(GuestPO user) {
+		sql = "insert into " + listName + " values (?,?,?)";
+		try {
+			conn = sqlconn.getConnection();
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, user.getId());
+			ps.setString(2, user.getType().toString());
+			ps.setString(3, user.getPassword());
+			ps.execute();
+			return Feedback.OPERATION_SUCCEED;
+		} catch (SQLException e) {
+			// e.printStackTrace();
+			return Feedback.OPERATION_FAIL;
+		}
+	}
+
+	public Feedback updateData(GuestPO user) {
+		sql = "update " + listName + " set userType=?, password=? where id=?";
+		try {
+			conn = sqlconn.getConnection();
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, user.getType().toString());
+			ps.setString(2, user.getPassword());
+			ps.setString(3, user.getId());
+			ps.execute();
+			return Feedback.OPERATION_SUCCEED;
+		} catch (SQLException e) {
+			// e.printStackTrace();
+			return Feedback.OPERATION_FAIL;
+		}
+	}
+
+	public ArrayList<GuestPO> getDataList() {
+		ArrayList<GuestPO> result = new ArrayList<GuestPO>();
+		sql = "select * from " + listName;
+		try {
+			conn = sqlconn.getConnection();
+			ps = conn.prepareStatement(sql);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				GuestPO r = new GuestPO();
+				r.setId(rs.getString(1));
+				r.setType(UserType.valueOf(rs.getString(2)));
+				r.setPassword(rs.getString(3));
+				result.add(r);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+}
---
+++ src/main/java/NJU/HouseWang/nju_eas_server/dataService/DataListService.java
@@ -0,0 +1,33 @@
+package NJU.HouseWang.nju_eas_server.dataService;
+
+import java.util.ArrayList;
+
+import NJU.HouseWang.nju_eas_server.po.DataPOService;
+import NJU.HouseWang.nju_eas_server.systemMessage.Feedback;
+
+public interface DataListService {
+	public void init();
+
+	public void finish();
+
+	// 得到数据实体
+	public DataPOService getData(String list, String id);
+
+	// 删除数据实体
+	public Feedback removeData(String list, String id);
+
+	// 加入数据实体
+	public Feedback addData(String list, DataPOService data);
+
+	// 更新数据实体
+	public Feedback updateData(String list, DataPOService data);
+
+	// 判断数据ID是否存在
+	public boolean containsID(String list, String id);
+
+	// 判断数据对象是否存在
+	public boolean containsData(String list, DataPOService data);
+
+	// 得到数据列表
+	public ArrayList<? extends DataPOService> getDataList(String list);
+}
