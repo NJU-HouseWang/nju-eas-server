4b65f1b6701326326c21589bd661d08c65119249
wxppt <wx_ppt@126.com>
2013-12-23 18:38:12 +0800

    服务器界面

--- src/main/java/NJU/HouseWang/nju_eas_server/Launcher.java
+++ src/main/java/NJU/HouseWang/nju_eas_server/Launcher.java
@@ -1,10 +1,6 @@
 package NJU.HouseWang.nju_eas_server;
 
-import java.awt.BorderLayout;
 import java.io.IOException;
-
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 import NJU.HouseWang.nju_eas_server.data.AuthorityManager;
 import NJU.HouseWang.nju_eas_server.net.Server;
@@ -24,16 +20,10 @@ public class Launcher {
 	 *            运行时参数
 	 */
 	public static void main(String[] args) {
+//		ServerUI serverUI = new ServerUI();
 		Server server = new Server();
 		AuthorityManager am = AuthorityManager.getInstance();
 		try {
-			JFrame frame = new JFrame();
-			JLabel lb = new JLabel("EAS Server is running now...");
-			frame.add(lb, BorderLayout.CENTER);
-			frame.pack();
-			frame.setLocation(400, 300);
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setVisible(true);
 			am.run();
 			server.start();
 		} catch (IOException e) {
